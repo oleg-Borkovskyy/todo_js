6d0ef06b0693398d1d77668769a3f4ea9b157ec4
Oleg Borkovskyi <olegbnpay@gmail.com>
2021-03-23 15:37:51 +0200

    change some logic and del var

--- index.js
+++ index.js
@@ -1,66 +1,45 @@
-const addBtn=document.querySelector('.addBtn');
-var close = document.getElementsByClassName("close");
-var myNodelist = document.getElementsByTagName("li");
-var list = document.querySelector('ul');
+const addBtn = document.querySelector('.addBtn');
+const listArr = [];
 
-
-
-const delListItem =(el)=>{
-    el.target.parentElement.remove();
-}
-const hasInnerText=(colection,val)=>{
-    let result=false;
-    for (let i = 0; i < colection.length; i++) {
-        if (colection[i].childNodes[0].data.toLowerCase()===val.toLowerCase()) {
-            return true;
-        } else {
-            result=false;
-        }
-    }
-    return result;
-}
-const newElement = () =>{
-    var li = document.createElement("li");
-    var inputValue = document.getElementById("myInput").value;
-    var t = document.createTextNode(inputValue);
-    let Nodelist = document.getElementsByTagName("li");
-    console.log(Nodelist);
-    li.appendChild(t);
-    if (inputValue === '') {
-      alert("You must write something! or list alredy has this item");
-    } else if(hasInnerText(Nodelist,inputValue)){
-        alert("list alredy has this item");
-    }else {
-        document.getElementById("myUl").appendChild(li);
-    }
-    document.getElementById("myInput").value = "";
-  
-    var span = document.createElement("SPAN");
-    var txt = document.createTextNode("\u00D7");
-    span.className = "close";
-    span.appendChild(txt);
-    li.appendChild(span);
-  
-    for (i = 0; i < close.length; i++) {
-      close[i].onclick = delListItem;
-    }
-  }
-
-const check=(el)=>{
-    if (el.target.tagName === 'LI') {
-        el.target.classList.toggle('checked');
-      }
+const delListItem = (el) => {
+  el.target.parentElement.remove();
 };
-addBtn.addEventListener("click",newElement,false);
-list.addEventListener('click', check, false);
-
-for (let i = 0; i < myNodelist.length; i++) {
-  var span = document.createElement("SPAN");
-  var txt = document.createTextNode("\u00D7");
-  span.className = "close";
+const createCloseBtn = (listEl) => {
+  let span = document.createElement('SPAN');
+  let txt = document.createTextNode('\u00D7');
+  span.className = 'close';
   span.appendChild(txt);
-  myNodelist[i].appendChild(span);
-}
+  span.onclick = delListItem;
+  listEl.appendChild(span);
+};
+const checkedItem = (el) => {
+  if (el.target.tagName === 'LI') {
+    el.target.classList.toggle('checked');
+  }
+};
 
+const newElement = () => {
+  const inputValue = document.getElementById('myInput').value;
+  const isInList = listArr.some((el) => {
+    return el.text === inputValue;
+  });
+  if (inputValue === '') {
+    alert('You must write something!');
+  } else if (isInList) {
+    alert('list alredy has this item');
+  } else {
+    const t = document.createTextNode(inputValue);
+    const newListItem = {
+      item: document.createElement('li'),
+      text: inputValue,
+    };
+    listArr.push(newListItem);
+    newListItem.item.appendChild(t);
+    createCloseBtn(newListItem.item);
+    newListItem.item.addEventListener('click', checkedItem, false);
+    document.getElementById('myUl').appendChild(newListItem.item);
+  }
+  document.getElementById('myInput').value = '';
+};
 
-
+addBtn.addEventListener('click', newElement, false);
